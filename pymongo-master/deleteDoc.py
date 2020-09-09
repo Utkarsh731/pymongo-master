f6cfab445f7eee19134fb96fc20e0bca1e1c6152
UTKARSH <uttkarsh.shuklas1996@gmail.com>
2020-09-09 10:20:42 +0530

    connection file added

--- pymongo-master/deleteDoc.py
+++ pymongo-master/deleteDoc.py
@@ -1,10 +1,14 @@
 import pymongo
+from bson import ObjectId
+
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 
 mydb = myclient["shop"] #shop=db name
 
 mycol=mydb["product"] #product=collection name
 
-#mycol.insert_one({"name":"A Pen","price":2})
+query={'_id':ObjectId("5f57249b1c380c2ae186af9f")}
+
+print(mycol.delete_one(query))
 
 for i in mycol.find():print(i)

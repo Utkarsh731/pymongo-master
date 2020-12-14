d5dcde160d261aa6670d064ac76a2baa4da5612c
UTKARSH <uttkarsh.shuklas1996@gmail.com>
2020-12-14 12:24:29 +0530

    removed

--- pymongo-master/akki.py
+++
@@ -1,5 +0,0 @@
-import re
-text="as"
-i={"email":"af","firstName":"adgd","lastName":"As"}
-if re.search(text,i["email"],re.IGNORECASE) or re.search(text,i["firstName"],re.IGNORECASE) or re.search(text,i["lastName"],re.IGNORECASE):
-              print("true")
